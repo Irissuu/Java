5ef2aa6edbc911fb4e28504bbe3171ba3fc7bbbd
Iris <161527170+Irissuu@users.noreply.github.com>
2024-06-17 18:08:55 -0300

    calculaing division with oop

---
+++ Java/CalcOop/src/CalcOop.java
@@ -0,0 +1,11 @@
+public class CalcOop {
+
+    double num1;
+    double num2;
+
+    public double calc(double num1, double num2){
+        double math = num1 / num2;
+        return math;
+
+    }
+}
