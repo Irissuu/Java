aa4fb955ffed566b2f39c6fb9f639210947ed855
Iris <161527170+Irissuu@users.noreply.github.com>
2024-07-15 23:04:07 -0300

    default modifier

---
+++ Java/Default/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+    ModifierDefault name = new ModifierDefault();
+    name.name = "Iris";
+        System.out.println("The name is: " + name.name);
+    }
+}
---
+++ Java/Default/src/ModifierDefault.java
@@ -0,0 +1,4 @@
+public class ModifierDefault {
+
+    String name;
+}
